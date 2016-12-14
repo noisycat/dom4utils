02f95d07351ed821591d53244f41f356c6a97faa
Charles <romanticfool@gmail.com>
2016-12-13 23:39:34 -0600

    New GUI rules... might just be a replication of MapEditor, but I wanted to abstract away the GUI and not load it if you just wanted to use the text features

---
+++ map_utility/MapGUI.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+''' module: MapGUI '''
+import subprocess
+
+currentMap = None
+
+def display(mapToOpen=currentMap):
+    ''' starts the mapEditor GUI'''
+
+    return None
+
+def startASYNC(mapToOpen=currentMap):
+    retval = subprocess.Open(display,shell=False)
+    return retval
+
+def start(mapToOpen=currentMap):
+    return None
